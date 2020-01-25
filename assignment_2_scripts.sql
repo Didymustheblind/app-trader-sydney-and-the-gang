557efe88ce4b8eed1b0c38c6d09fe1af238ceaa6
Adam Tsuchiyama <adamtsuch21@gmail.com>
2020-01-25 10:47:01 -0600

    Top 10 list

--- assignment_2_scripts.sql
+++ assignment_2_scripts.sql
@@ -1,12 +1,10 @@
 
---Genre 'Food & Drink': Starbucks, DoorDash
---Genre 'Shopping': Wish
 --Genre 'Games': Fernanfloo, Narcos: Cartel Wars, Solitaire
 
 SELECT DISTINCT a.name, ROUND(((a.rating + p.rating)/ 2), 2) AS avg_rating,
 a.price AS app_price, p.price AS play_price, a.primary_genre AS app_genre,
 p.genres AS play_genre, p.install_count, a.content_rating AS a_content_rating,
-p.content_rating AS a_content_rating
+p.content_rating AS p_content_rating
 FROM app_store_apps AS a
 INNER JOIN play_store_apps AS p
 ON a.name = p.name
